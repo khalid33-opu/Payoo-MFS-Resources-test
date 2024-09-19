d3d3822d85a0432e1c06d1afd17214a408cc3faf
khalid33-opu <khalidmahmudopu@gmail.com>
2024-09-19 17:05:12 +0600

    added card

--- js/login.js
+++ js/login.js
@@ -1,9 +1,20 @@
 // console.log('button click by added');
 
 document.getElementById('button-login')
-    .addEventListener('click', function(event){
+    .addEventListener('click', function (event) {
         event.preventDefault();
         console.log('login button click');
         const phoneNumber = document.getElementById('phone-number').value;
-        console.log(phoneNumber);
+        const pinNumber = document.getElementById('pin-number').value;
+        console.log(phoneNumber, pinNumber);
+
+        if (phoneNumber === '5' && pinNumber === '1234') {
+            console.log('you are login');
+            window.location.href = '/home.html';
+        }
+        else {
+            alert('wrong phone number or pin')
+        }
+
+
     })
