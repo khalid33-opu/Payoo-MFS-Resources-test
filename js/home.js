556a83c0ea4d27f6c98aab0a5aedb691b0b5880a
khalid33-opu <khalidmahmudopu@gmail.com>
2024-09-20 10:52:52 +0600

    added some section

--- js/home.js
+++ js/home.js
@@ -1,1 +1,39 @@
-console.log('home added');
+// add money to the account
+document.getElementById('btn-add-money')
+    .addEventListener('click', function (event) {
+
+        // page reload
+
+        event.preventDefault();
+        console.log('money add button click');
+
+        const inputAddMoney = document.getElementById('input-add-money').value;
+        console.log(inputAddMoney);
+
+        // get the pin number
+
+        const pinNumberInput = document.getElementById('input-pin-number')
+        console.log(pinNumberInput);
+
+        // verify pin
+
+        if (pinNumberInput === '1234') {
+            console.log('adding money to your account');
+
+
+            // grt the currant balance
+
+            const balance = document.getElementById('account-balance').innerText;
+            console.log(balance);
+
+            // add money input with money
+            const newBalance = balance + inputAddMoney ;
+            console.log(newBalance);
+
+        }
+        else {
+            alert('failed to add money ! please tey again')
+        }
+
+
+    })
